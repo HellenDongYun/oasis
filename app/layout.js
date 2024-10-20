931eb4e87d315679b4c37de57fb74ed20bcf59d2
hellen <hellendongyun@gmail.com>
2024-10-20 22:13:16 +1300

    tailwind styling

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
-import Logo from "@/components/Logo";
 import Footer from "@/components/footer";
-import Navigation from "@/components/navigation";
 import "/globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Header from "@/components/Header";
 export const metadata = {
   title: {
     template: "%s The Oasis",
@@ -16,19 +15,16 @@ const josefin = Josefin_Sans({
   variable: "--font-josefin",
   display: "swap",
 });
-console.log(josefin);
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={` ${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={` ${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation></Navigation>
-        </header>
-        <main>{children}</main>
-        <Footer />
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl bg-red-500 mx-auto ">{children}</main>
+        </div>
       </body>
     </html>
   );
