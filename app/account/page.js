029084497e8771163f0b6d9b83a5c9462be5049e
hellen <hellendongyun@gmail.com>
2024-10-24 21:34:58 +1300

    login

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,15 @@
+import { auth } from "@/lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
-const AccountPage = () => {
+const AccountPage = async () => {
+  const session = await auth();
+  const firstName = session.user.name.split(" ")[0];
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, You !
+      Welcome, {firstName}!
     </h2>
   );
 };
